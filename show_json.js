d48177c7172188d742cde926f0ab3e888ca402aa
jojododoe1212 <jojododoe1212@gmail.com>
2020-06-25 02:28:31 -0500

    add total cases

--- show_json.js
+++ show_json.js
@@ -7,14 +7,17 @@ window.onload = function loadServers(){
 		{
 			var data = JSON.parse(this.responseText);
 			var table = "";
+			var total = 20;
 			
 			for(var i = 0; i < data.length; i++)
 			{
 				table += "<tr><td>" + data[i].id + "</td><td>"
 					  + data[i].date + "</td><td>"
 					  + data[i].cases + "</td></tr>";
+			  	total +=data[i].cases;
 				document.getElementById("sortable").innerHTML = table;		
-			}	
+			}
+			document.getElementById("total").insertAdjacentHTML("beforeend",total);	
 		}
 	}
 	xHttp.send();
