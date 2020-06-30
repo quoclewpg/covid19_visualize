a8a7bd4724b1ee269a4fe50681efb2a02af0151b
jojododoe1212 <jojododoe1212@gmail.com>
2020-06-30 03:04:42 -0500

    add days of the wek

--- show_json.js
+++ show_json.js
@@ -8,14 +8,20 @@ window.onload = function loadServers(){
 			var data = JSON.parse(this.responseText);
 			var table = "";
 			var total = 20;
-			
+			var day = 10;
+			var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+			var d;
+
 			for(var i = 0; i < data.length; i++)
 			{
-				table += "<tr><td>" + i + 1 + "</td><td id='dates'>"
+				d = new Date(data[i].date);
+				table += "<tr><td>" + day + "</td><td>"
+					  + days[d.getDay()] + "</td><td id='dates'>"
 					  + data[i].date + "</td><td>"
 					  + data[i].cases + "</td></tr>";
 			  	total +=data[i].cases;
-				document.getElementById("sortable").innerHTML = table;		
+				document.getElementById("sortable").innerHTML = table;	
+				day++;	
 			}
 			document.getElementById("total").insertAdjacentHTML("beforeend",total);	
 		}
